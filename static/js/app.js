49f5da7abd3ccfda419d928a1e4749e1c41db2f7
tony <anthony.garvan@gmail.com>
2013-12-01 20:00:16 -0600

    Added privacy policy and contact us pages, also resolved active state regression for navigation menu.

--- static/js/app.js
+++ static/js/app.js
@@ -4,13 +4,17 @@ App.Router.map(function() {
     this.route("index", { path: "/" });
     this.route("how-it-works", { path: "/how-it-works" });
     this.route("buy-an-ocho", { path: "/buy-an-ocho" });
+    this.route("privacy-policy", { path: "/privacy-policy" });
+    this.route("contact-us", { path: "/contact-us" });
 });
 
+/*
 App.IndexRoute = Ember.Route.extend({
   renderTemplate: function() {
     this.render('index');
   }
 });
+*/
 
 App.IndexView = Ember.View.extend({
   didInsertElement : function(){
@@ -19,8 +23,16 @@ App.IndexView = Ember.View.extend({
         $("#slides").slidesjs({
             width: 800,
             height: 550,
-            navigation: false
+            navigation: false,
+            play: {
+              active: false,
+              auto: true,
+              interval: 4000,
+              swap: false
+            }
           })
         $('#slides').css('overflow', 'visible');
+        //$('.slidesjs-play').css('display', 'none');
+        //$('.slidesjs-stop').css('display', 'none');
     });}
 });
