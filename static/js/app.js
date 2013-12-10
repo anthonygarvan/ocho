3a012f677a3105881f28172096ea4f7055b709f9
tony <anthony.garvan@gmail.com>
2013-12-09 22:09:53 -0600

    Chartjs integrated into dashboard (no formatting or data).

--- static/js/app.js
+++ static/js/app.js
@@ -123,6 +123,36 @@ App.PortalDevicesView = Ember.View.extend({
             $(selector).hide();})
     });}
 });
+
+App.PortalIndexView = Ember.View.extend({
+  didInsertElement : function(){
+    this._super();
+    Ember.run.scheduleOnce('afterRender', this, function(){
+        var lineChartData = {
+			labels : ["January","February","March","April","May","June","July"],
+			datasets : [
+				{
+					fillColor : "rgba(220,220,220,0.5)",
+					strokeColor : "rgba(220,220,220,1)",
+					pointColor : "rgba(220,220,220,1)",
+					pointStrokeColor : "#fff",
+					data : [65,59,90,81,56,55,40]
+				},
+				{
+					fillColor : "rgba(151,187,205,0.5)",
+					strokeColor : "rgba(151,187,205,1)",
+					pointColor : "rgba(151,187,205,1)",
+					pointStrokeColor : "#fff",
+					data : [28,48,40,19,96,27,100]
+				}
+			]
+
+		}
+
+	var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Line(lineChartData);
+    });}
+});
+
 var toggleLogin = function() {
         if($('#login-form').is(":visible")) {
             $('#login-form').slideUp();
